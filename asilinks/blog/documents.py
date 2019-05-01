06ca21f4cd8fd3729ac3cf7981be08e75bdee425
Ricardo Leon <ricardoleoncorreia@gmail.com>
2019-05-01 18:47:13 -0300

    Implement author_image endpoint

--- asilinks/blog/documents.py
+++ asilinks/blog/documents.py
@@ -1,5 +1,7 @@
 from django.core.files.base import ContentFile
 from mongoengine import fields, document
+from asilinks.fields import LocalStorageFileField
+from asilinks.storage_backends import PublicOverrideMediaStorage
 
 class Article(document.Document):
     active = fields.BooleanField(default=True)
@@ -7,3 +9,5 @@ class Article(document.Document):
     title = fields.StringField(max_length=500)
     author = fields.StringField(max_length=255)
     draft = fields.BooleanField(default=False)
+    author_image = LocalStorageFileField(upload_to='blog/',
+        default='blog/default.png', storage=PublicOverrideMediaStorage())
--- asilinks/blog/serializers.py
+++ asilinks/blog/serializers.py
@@ -6,9 +6,45 @@ from rest_framework import serializers, fields
 from rest_framework.exceptions import ValidationError
 from rest_framework_mongoengine.serializers import DocumentSerializer
 
+# Custom imports
+from asilinks.validators import file_max_size
+
 
 class ArticleSerializer(DocumentSerializer):
+
+    author_image = fields.ImageField(use_url=True, required=False, 
+        default='blog/default.png', validators=[file_max_size])
 
     class Meta:
         model = Article
         fields = '__all__'
+
+    def create(self, validated_data):
+
+        instance = Article(**validated_data)
+        instance.save()
+
+        # if 'article_image' in validated_data:
+        #     name = 'asi-{}.{}'.format(instance.id, validated_data['article_image'].name.split('.')[-1])
+        #     instance.article_image.save(name, validated_data.get('article_image'))
+
+        if 'author_image' in validated_data:
+            name = 'asi-{}.{}'.format(instance.id, validated_data['author_image'].name.split('.')[-1])
+            instance.author_image.save(name, validated_data.get('author_image'))
+        
+        return instance
+
+
+    def update(self, instance, validated_data):
+        
+        # instance.save()
+
+        # if 'article_image' in validated_data:
+        #     name = 'asi-{}.{}'.format(instance.id, validated_data['article_image'].name.split('.')[-1])
+        #     instance.article_image.save(name, validated_data.get('article_image'))
+        
+        if 'author_image' in validated_data:
+            name = 'asi-{}.{}'.format(instance.id, validated_data['author_image'].name.split('.')[-1])
+            instance.author_image.save(name, validated_data.get('author_image'))
+        
+        return instance
